29fc0b1f621ab336798daf6238c7c569652fb621
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-13 18:16:13 -0300

    feat: :sparkles: configure actions after signin

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import { nanoid } from 'nanoid';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { db } from '../../../services/firebase';
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,27 @@ export default NextAuth({
     }),
   ],
   secret: process.env.NEXT_AUTH_SECRET,
+  callbacks: {
+    async signIn({ user: { email } }) {
+      try {
+        const duplicatesQuery = query(
+          collection(db, 'users'),
+          where('email', '==', email)
+        );
+        const querySnapshot = await getDocs(duplicatesQuery);
+        const [data] = querySnapshot.docs.map((doc) => doc.data());
+
+        if (!data) {
+          await addDoc(collection(db, 'users'), {
+            email,
+            ts: new Date(),
+            id: nanoid(),
+          });
+        }
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });

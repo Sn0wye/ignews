8a8f2dfcfdf0798196bea0b50e7405e06db5cb7c
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-13 13:36:57 -0300

    feat: :sparkles: add functionality to button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
 
 const SignInButton = () => {
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const { data: session, status } = useSession();
 
-  return isUserLoggedIn ? (
-    <button type='button' className={styles.signInButton}>
+  console.log(session);
+
+  return session ? (
+    <button
+      type='button'
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color='#04d361' />
-      Gabriel Trzimajewski
+      {session.user?.name}
       <FiX color='#737380' className={styles.closeIcon} />
     </button>
   ) : (
-    <button type='button' className={styles.signInButton}>
+    <button
+      type='button'
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color='#8257e6' />
       Sign in with Github
     </button>

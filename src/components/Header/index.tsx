76493e4c20ac660d307e5c872ce03c62df3fe0bd
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-16 19:24:06 -0300

    refactor: :fire: prefetch deprecated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -17,7 +17,7 @@ const Header = () => {
           <ActiveLink href='/' activeClassName={styles.active}>
             <a>Home</a>
           </ActiveLink>
-          <ActiveLink href='/posts' activeClassName={styles.active} prefetch>
+          <ActiveLink href='/posts' activeClassName={styles.active}>
             <a>Posts</a>
           </ActiveLink>
         </nav>

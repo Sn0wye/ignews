a89cdb5e39c45d3cb4a8b239fb82e7a894790edd
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-16 12:22:37 -0300

    feat: :lipstick: base list styling and mock done

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,43 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+const Posts = () => {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.postList}>
+          <a href='#'>
+            <time>March, 12, 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+          <a href='#'>
+            <time>March, 12, 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+          <a href='#'>
+            <time>March, 12, 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages with a shared build, test, and release process.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Posts;

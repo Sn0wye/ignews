0b879b3e7a63fcc70436c585770ed198f92e9895
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-16 19:49:40 -0300

    feat(auth): :sparkles: retrive user subscription inside session info

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -11,6 +11,35 @@ export const authOptions: NextAuthOptions = {
   ],
   secret: process.env.NEXT_AUTH_SECRET,
   callbacks: {
+    async session({ session }) {
+      try {
+        const loggedUser = await prisma.user.findUniqueOrThrow({
+          where: {
+            email: session.user?.email?.toString(),
+          },
+          select: {
+            id: true,
+          },
+        });
+
+        const [userActiveSubscription] = await prisma.subscription.findMany({
+          where: {
+            userId: loggedUser.id,
+            status: 'active',
+          },
+        });
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
     async signIn({ user: { email } }) {
       try {
         const userExists = await prisma.user.findUnique({
